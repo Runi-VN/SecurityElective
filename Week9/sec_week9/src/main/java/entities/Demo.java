c9286492e24492d5102281713417b2aa1a07b970
Runi <Runiniclasen@hotmail.com>
2020-03-26 15:37:09 +0100

    push w9

---
+++ Week9/sec_week9/src/main/java/entities/Demo.java
@@ -0,0 +1,85 @@
+
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Runi
+ */
+@Entity
+public class Demo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String info;
+
+    public Demo(String info) {
+        this.info = info;
+    }
+
+    public Demo() {
+    }
+    
+    
+
+    /**
+     * Get the value of info
+     *
+     * @return the value of info
+     */
+    public String getInfo() {
+        return info;
+    }
+
+    /**
+     * Set the value of info
+     *
+     * @param info new value of info
+     */
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Demo)) {
+            return false;
+        }
+        Demo other = (Demo) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entities.demo[ id=" + id + " ]";
+    }
+    
+}
