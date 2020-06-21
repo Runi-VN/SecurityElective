c43514ba253953959120a237929cbe55dbec28df
Rúni Vedel Niclasen <runiniclasen@hotmail.com>
2020-06-21 15:04:05 +0200

    push encrypt example

--- Week4/src/main/java/dk/cphbusiness/security/encryption/Exercise2.java
+++ Week4/src/main/java/dk/cphbusiness/security/encryption/Exercise2.java
@@ -2,6 +2,10 @@ package dk.cphbusiness.security.encryption;
 
 import java.util.Base64;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Base64.Encoder;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
@@ -16,7 +20,7 @@ public class Exercise2 {
     private static final String CIFER
             = "NmxhSedpfrayg4OsgKNWSjVp68E0xa76H5bOa+LEgt3fvVWPM/QHX48ySecVpyEPO/xVRaa2URbzEglWugmPpji8Q6ClwoMYHmX6qtimZ7I=";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
 
         for (int i = 1000; i <= 10000; i++) {
             String key = "passwordabcd" + i; // has to be 128 bit/16 bytes
@@ -30,9 +34,28 @@ public class Exercise2 {
             if (dec != null && dec.contains("everything")) {
                 System.out.println("Decrypted: " + dec);
                 System.out.println("Password value: " + i);
+                System.out.println("---------\nEncryption example");
+                String toEncrypt = "This is my secret data abcd1234$€";
+                String myKey = generateSafeToken();
+                String encrypted = encrypt(toEncrypt, myKey);
+                String decrypted = decrypt(encrypted, myKey);
+                System.out.println("Data to encrypt: " + toEncrypt);
+                System.out.println("Key to encrypt with: " + myKey);
+                System.out.println("Encrypted result: " + encrypted);
+                System.out.println("Decrypted result: " + decrypted);
                 break;
             }
+
         }
+    }
+
+    private static String generateSafeToken() {
+        SecureRandom random = new SecureRandom();
+        byte bytes[] = new byte[18];
+        random.nextBytes(bytes);
+        Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+        String token = encoder.encodeToString(bytes);
+        return token;
     }
 
     public static String encrypt(final String valueEnc, final String secKey) {
@@ -53,22 +76,19 @@ public class Exercise2 {
         String decryptedValue = null;
         try {
             final Key key = generateKeyFromString(secretKey);
-            final Cipher c = Cipher.getInstance(ALGORITHM);
-            c.init(Cipher.DECRYPT_MODE, key);
+            final Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, key);
             final byte[] decorVal = Base64.getDecoder().decode(encryptedValue);
-            // System.out.println(decorVal.length);
-            final byte[] decValue = c.doFinal(decorVal);
+            final byte[] decValue = cipher.doFinal(decorVal);
             decryptedValue = new String(decValue);
         } catch (Exception ex) {
             System.out.println("The Exception is=" + ex);
-            // ex.printStackTrace();
         }
         return decryptedValue;
     }
 
     private static Key generateKeyFromString(final String secKey) throws Exception {
         final byte[] keyVal = secKey.getBytes();
-        final Key key = new SecretKeySpec(keyVal, ALGORITHM);
-        return key;
+        return new SecretKeySpec(keyVal, ALGORITHM); //generates an AES key from a byte array
     }
 }
